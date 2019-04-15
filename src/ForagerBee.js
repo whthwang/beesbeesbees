e7b8f05414de6f960f298360a09965cf15ec16b9
wayne hwang <whthwang@gmail.com>
2019-04-15 11:57:50 -0700

    committing all tests passed for each bee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
 var ForagerBee = function() {
+  this.age = 10;
+  this.job = 'find pollen';
+  this.color = 'yellow'
+  this.canFly = true;
+  this.treasureChest = [];
+
+
 };
 
+ForagerBee.prototype = new Bee();
+ForagerBee.prototype.forage = function () {
+  this.treasureChest.push('treasure');
+}
+ForagerBee.prototype.constructor = ForagerBee;
